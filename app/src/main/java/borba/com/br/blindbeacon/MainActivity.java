8abe3e6b5adca02151bb0cb62d214b211fc09a31
André Borba <andreborba.92@gmail.com>
2017-04-17 23:28:51 -0300

    Melhorias de layout

--- app/src/main/java/borba/com/br/blindbeacon/MainActivity.java
+++ app/src/main/java/borba/com/br/blindbeacon/MainActivity.java
@@ -15,6 +15,7 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
 import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.Toast;
@@ -41,7 +42,10 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+        //getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.window_title);
 
         this.ctx = this;
 
